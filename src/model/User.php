d8a775528a06b02beed3191481e3ff61bbf31dc6
leanazulyoro <leanazulyoro@gmail.com>
2016-10-26 23:56:19 -0300

    Added: User model

--- src/model/User.php
+++ src/model/User.php
@@ -1,14 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lalmeida
- * Date: 26/10/16
- * Time: 23:40
- */
 
-namespace olxtest;
+namespace olxtest\model;
 
+
+use olxtest\helper\data_type_validator\DataTypeValidator;
 
 class User {
+  private $id;
+  private $name;
+  private $picture;
+  private $address;
+
+  function __construct($id, $name, $picture, $address) {
+    $this->id = DataTypeValidator::isIntegerOrThrow($id);
+    $this->name = DataTypeValidator::isStringOrThrow($name);
+    $this->picture = DataTypeValidator::isStringOrThrow($picture);
+    $this->address = DataTypeValidator::isStringOrThrow($address);
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getPicture() {
+    return $this->picture;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getAddress() {
+    return $this->address;
+  }
 
 }
